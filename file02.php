13cb4839f4a5a54e14d5614a0d819d91c71282f7
ChihTai <49051958+ChihTai@users.noreply.github.com>
2019-05-13 14:43:07 +0800

    teacher's file02.php
    teacher's file02.php

--- file02.php
+++ file02.php
@@ -5,8 +5,12 @@
 2.input type="file"
 3.檔案以二進位方式傳輸到暫存目錄中
 4.以$_FILES來存取相關的屬性
-  ->$_FILES["file"]['name'] 上傳檔案的原始名稱
+  ->$_FILES["file"]["name"] 上傳檔案的原始名稱
   ->$_FILES["file"]["type"] 上傳檔案的檔案類型
+    =>"image/gif"
+    =>"image/jpeg"
+    =>"image/jpg"
+    =>"image/png"
   ->$_FILES["file"]["size"] 上傳檔案的原始大小
   ->$_FILES["file"]["tmp_name"] 上傳檔案的暫存位置
   ->$_FILES["file"]["error"] 錯誤代碼
@@ -16,38 +20,44 @@
 ***************************************************/
 
 //練習 上傳檔案後提供下載，檔案路徑存在資料表中
-/* if(!empty($_POST)){
-  echo $_FILES['name'];
-} */
 $dsn="mysql:host=localhost;charset=utf8;dbname=shop";
 $pdo=new PDO($dsn,"root","");
 
 //利用暫存路徑來判斷是否有上傳檔案
 if(!empty($_FILES["pic"]["tmp_name"]) && $_FILES["pic"]["type"]=='image/png'){
-//if(!empty($_FILES["pic"]["tmp_name"]) ){
-
   
-/* echo $_FILES['pic']['name'];
-echo $_FILES["pic"]["tmp_name"]; */
+/*   echo $_FILES['pic']['name'];
+  echo $_FILES["pic"]["tmp_name"]; */
 
-$name=$_FILES['pic']['name'];
-$path=$_FILES["pic"]["tmp_name"];
+  $name=$_FILES['pic']['name'];
+  $path=$_FILES["pic"]["tmp_name"];
 
-move_uploaded_file($path,"./img/" . $name);
+  //移動檔案到指定目錄下,並改命為$name
+  move_uploaded_file($path,"./img/" . $name);
 
-echo "<img src='./img/$name' style='width:30px'>";
 
-$sql="insert into img (`name`,`path`) values('$name','./img/$name')";
-$pdo->query($sql);
-} 
+  $sql="insert into img (`name`,`path`) values('$name','./img/$name')";
+  $pdo->query($sql);
+  
+}
+
+
 
 ?>
-
+<style>
+table{
+  border-collapse:collapse;
+}
+td{
+  text-align:center;
+  padding:5px;
+  border:1px solid #ccc;
+}
+</style>
 <form action="?" method="post" enctype="multipart/form-data">
-<input type="file" name="pic">
-
-<input type="submit" value="上傳" >
-
+  <input type="file" name="pic">
+  
+  <input type="submit" value="上傳">
 </form>
 <table>
   <tr>
